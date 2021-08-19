6fffaeaae18ae715c6c5bf7492e84abfc6e94a94
Vaios Kalpias Ilias <vkalpias@gmail.com>
2021-08-19 13:31:52 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import Events from './src/events';
+import Observer from './src/observer';
+import ObserverList from './src/observer-list';
+import History from './src/history';
+import ObserverHistory from './src/observer-history';
+
+export {
+    Events,
+    Observer,
+    ObserverList,
+    History,
+    ObserverHistory
+};
---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+const main = {
+    input: 'index.js',
+    output: {
+        file: 'dist/index.js',
+        format: 'umd',
+        name: 'observer'
+    }
+};
+
+let targets = [
+    main
+];
+
+export default targets;
