e6182c85daa9439e0c38173d075a7cffaea566c2
Vaios Kalpias Ilias <vkalpias@gmail.com>
2021-08-31 11:19:44 +0300

    Publish umd and es6 modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-const main = {
+const umd = {
     input: 'index.js',
     output: {
         file: 'dist/index.js',
@@ -7,8 +7,21 @@ const main = {
     }
 };
 
-let targets = [
-    main
-];
+const es6 = {
+    input: 'index.js',
+    output: {
+        file: 'dist/index.mjs',
+        format: 'module'
+    }
+};
+
+let targets;
+if (process.env.target) {
+    switch (process.env.target.toLowerCase()) {
+        case "umd":      targets = [umd]; break;
+        case "es6":      targets = [es6]; break;
+        case "all":      targets = [umd, es6]; break;
+    }
+}
 
 export default targets;
