364caac451192a4b792baff1ffffab0a5e8b6848
Will Eastcott <will@playcanvas.com>
2021-11-05 13:24:56 +0000

    Migrate codebase to ES6 classes

---
+++ src/event-handle.js
@@ -0,0 +1,50 @@
+/**
+ * @class
+ * @name EventHandle
+ * @param {Events} owner - Owner
+ * @param {string} name - Name
+ * @param {HandleEvent} fn - Callback function
+ */
+class EventHandle {
+    constructor(owner, name, fn) {
+        this.owner = owner;
+        this.name = name;
+        this.fn = fn;
+    }
+
+    /**
+     * @name EventHandle#unbind
+     */
+    unbind() {
+        if (! this.owner)
+            return;
+
+        this.owner.unbind(this.name, this.fn);
+
+        this.owner = null;
+        this.name = null;
+        this.fn = null;
+    }
+
+    /**
+     * @name EventHandle#call
+     */
+    call() {
+        if (! this.fn)
+            return;
+
+        this.fn.call(this.owner, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6], arguments[7]);
+    }
+
+    /**
+     * @name EventHandle#on
+     * @param {string} name - Name
+     * @param {HandleEvent} fn - Callback function
+     * @returns {EventHandle} - EventHandle
+     */
+    on(name, fn) {
+        return this.owner.on(name, fn);
+    }
+}
+
+export default EventHandle;
--- src/events.js
+++ src/events.js
@@ -1,3 +1,5 @@
+import EventHandle from './event-handle.js';
+
 /**
  * @callback HandleEvent
  * @description Callback used by {@link Events} and {@link EventHandle} functions. Note the callback is limited to 8 arguments.
@@ -15,207 +17,161 @@
  * @class
  * @name Events
  */
-function Events() {
-    // _world
-    Object.defineProperty(
-        this,
-        '_events', {
-            enumerable: false,
-            configurable: false,
-            writable: true,
-            value: { }
-        }
-    );
-
-    this._suspendEvents = false;
-
-    this._additionalEmitters = [];
-
-    Object.defineProperty(this, 'suspendEvents', {
-        get: function () {
-            return this._suspendEvents;
-        },
-        set: function (value) {
-            this._suspendEvents = !!value;
-        }
-    });
-}
-
-/**
- * @name Events#on
- * @param {string} name - Name
- * @param {HandleEvent} fn - Callback function
- * @returns {EventHandle} EventHandle
- */
-Events.prototype.on = function (name, fn) {
-    var events = this._events[name];
-    if (events === undefined) {
-        this._events[name] = [fn];
-    } else {
-        if (events.indexOf(fn) === -1)
-            events.push(fn);
-    }
-    return new EventHandle(this, name, fn);
-};
-
-/**
- * @name Events#once
- * @param {string} name - Name
- * @param {HandleEvent} fn - Callback function
- * @returns {EventHandle} EventHandle
- */
-Events.prototype.once = function (name, fn) {
-    var self = this;
-    var evt = this.on(name, function (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7) {
-        fn.call(self, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
-        evt.unbind();
-    });
-    return evt;
-};
-
-/**
- * @name Events#emit
- * @param {string} name - Name
- * @param {any} arg0 - First argument
- * @param {any} arg1 - Second argument
- * @param {any} arg2 - Third argument
- * @param {any} arg3 - Fourth argument
- * @param {any} arg4 - Fifth argument
- * @param {any} arg5 - Sixth argument
- * @param {any} arg6 - Seventh argument
- * @param {any} arg7 - Eights argument
- * @returns {Events} Self for chaining.
- */
-Events.prototype.emit = function (name, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7) {
-    if (this._suspendEvents) return;
-
-    var events = this._events[name];
-    if (events && events.length) {
-        events = events.slice(0);
-
-        for (var i = 0; i < events.length; i++) {
-            if (! events[i])
-                continue;
-
-            try {
-                events[i].call(this, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
-            } catch (ex) {
-                console.info('%c%s %c(event error)', 'color: #06f', name, 'color: #f00');
-                console.log(ex.stack);
+class Events {
+    constructor() {
+        // _world
+        Object.defineProperty(
+            this,
+            '_events', {
+                enumerable: false,
+                configurable: false,
+                writable: true,
+                value: { }
             }
-        }
+        );
+
+        this._suspendEvents = false;
+
+        this._additionalEmitters = [];
     }
 
-    if (this._additionalEmitters.length) {
-        const emitters = this._additionalEmitters.slice();
-        emitters.forEach((emitter) => {
-            emitter.emit(name, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
-        });
+    get suspendEvents() {
+        return this._suspendEvents;
     }
 
-    return this;
-};
+    set suspendEvents(value) {
+        this._suspendEvents = !!value;
+    }
 
-/**
- * @name Events#unbind
- * @param {string} name - Name
- * @param {HandleEvent} fn - Callback function
- * @returns {Events} - This
- */
-Events.prototype.unbind = function (name, fn) {
-    if (name) {
+    /**
+     * @name Events#on
+     * @param {string} name - Name
+     * @param {HandleEvent} fn - Callback function
+     * @returns {EventHandle} EventHandle
+     */
+    on(name, fn) {
         var events = this._events[name];
-        if (! events)
-            return this;
+        if (events === undefined) {
+            this._events[name] = [fn];
+        } else {
+            if (events.indexOf(fn) === -1)
+                events.push(fn);
+        }
+        return new EventHandle(this, name, fn);
+    }
 
-        if (fn) {
-            var i = events.indexOf(fn);
-            if (i !== -1) {
-                if (events.length === 1) {
-                    delete this._events[name];
-                } else {
-                    events.splice(i, 1);
+    /**
+     * @name Events#once
+     * @param {string} name - Name
+     * @param {HandleEvent} fn - Callback function
+     * @returns {EventHandle} EventHandle
+     */
+    once(name, fn) {
+        var self = this;
+        var evt = this.on(name, function (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7) {
+            fn.call(self, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+            evt.unbind();
+        });
+        return evt;
+    }
+
+    /**
+     * @name Events#emit
+     * @param {string} name - Name
+     * @param {any} arg0 - First argument
+     * @param {any} arg1 - Second argument
+     * @param {any} arg2 - Third argument
+     * @param {any} arg3 - Fourth argument
+     * @param {any} arg4 - Fifth argument
+     * @param {any} arg5 - Sixth argument
+     * @param {any} arg6 - Seventh argument
+     * @param {any} arg7 - Eights argument
+     * @returns {Events} Self for chaining.
+     */
+    emit(name, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7) {
+        if (this._suspendEvents) return;
+
+        var events = this._events[name];
+        if (events && events.length) {
+            events = events.slice(0);
+
+            for (var i = 0; i < events.length; i++) {
+                if (! events[i])
+                    continue;
+
+                try {
+                    events[i].call(this, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+                } catch (ex) {
+                    console.info('%c%s %c(event error)', 'color: #06f', name, 'color: #f00');
+                    console.log(ex.stack);
                 }
             }
-        } else {
-            delete this._events[name];
         }
-    } else {
-        this._events = { };
+
+        if (this._additionalEmitters.length) {
+            const emitters = this._additionalEmitters.slice();
+            emitters.forEach((emitter) => {
+                emitter.emit(name, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+            });
+        }
+
+        return this;
     }
 
-    return this;
-};
+    /**
+     * @name Events#unbind
+     * @param {string} name - Name
+     * @param {HandleEvent} fn - Callback function
+     * @returns {Events} - This
+     */
+    unbind(name, fn) {
+        if (name) {
+            var events = this._events[name];
+            if (! events)
+                return this;
 
-/**
- * Adds another emitter. Any events fired by this instance
- * will also be fired on the additional emitter.
- *
- * @param {Events} emitter - The emitter
- */
-Events.prototype.addEmitter = function (emitter) {
-    if (!this._additionalEmitters.includes(emitter)) {
-        this._additionalEmitters.push(emitter);
+            if (fn) {
+                var i = events.indexOf(fn);
+                if (i !== -1) {
+                    if (events.length === 1) {
+                        delete this._events[name];
+                    } else {
+                        events.splice(i, 1);
+                    }
+                }
+            } else {
+                delete this._events[name];
+            }
+        } else {
+            this._events = { };
+        }
+
+        return this;
     }
-};
 
-/**
- * Removes emitter.
- *
- * @param {Events} emitter - The emitter
- */
-Events.prototype.removeEmitter = function (emitter) {
-    const idx = this._additionalEmitters.indexOf(emitter);
-    if (idx !== -1) {
-        this._additionalEmitters.splice(idx, 1);
+    /**
+     * Adds another emitter. Any events fired by this instance
+     * will also be fired on the additional emitter.
+     *
+     * @param {Events} emitter - The emitter
+     */
+    addEmitter(emitter) {
+        if (!this._additionalEmitters.includes(emitter)) {
+            this._additionalEmitters.push(emitter);
+        }
     }
-};
 
-/**
- * @class
- * @name EventHandle
- * @param {Events} owner - Owner
- * @param {string} name - Name
- * @param {HandleEvent} fn - Callback function
- */
-function EventHandle(owner, name, fn) {
-    this.owner = owner;
-    this.name = name;
-    this.fn = fn;
+    /**
+     * Removes emitter.
+     *
+     * @param {Events} emitter - The emitter
+     */
+    removeEmitter(emitter) {
+        const idx = this._additionalEmitters.indexOf(emitter);
+        if (idx !== -1) {
+            this._additionalEmitters.splice(idx, 1);
+        }
+    }
 }
-
-/**
- * @name EventHandle#unbind
- */
-EventHandle.prototype.unbind = function () {
-    if (! this.owner)
-        return;
-
-    this.owner.unbind(this.name, this.fn);
-
-    this.owner = null;
-    this.name = null;
-    this.fn = null;
-};
-
-/**
- * @name EventHandle#call
- */
-EventHandle.prototype.call = function () {
-    if (! this.fn)
-        return;
-
-    this.fn.call(this.owner, arguments[0], arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6], arguments[7]);
-};
-
-/**
- * @name EventHandle#on
- * @param {string} name - Name
- * @param {HandleEvent} fn - Callback function
- * @returns {EventHandle} - EventHandle
- */
-EventHandle.prototype.on = function (name, fn) {
-    return this.owner.on(name, fn);
-};
 
 export default Events;
