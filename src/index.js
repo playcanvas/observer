3d6b17b80ff7a06febf4dd30a05df2445304ee53
Will Eastcott <will@playcanvas.com>
2024-01-21 13:44:01 +0000

    Switch to tsc to generate types

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Events from './events.js';
+import Observer from './observer.js';
+import ObserverList from './observer-list.js';
+import History from './history.js';
+import ObserverHistory from './observer-history.js';
+
+export {
+    Events,
+    Observer,
+    ObserverList,
+    History,
+    ObserverHistory
+};
