0a4b9f3628fb8d41b33c53483e5c733ee89cdc93
kpal <kpal81xd@gmail.com>
2025-09-03 18:06:21 +0100

    Update ESLint configuration to target TypeScript files instead of MJS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import globals from 'globals';
 export default [
     ...playcanvasConfig,
     {
-        files: ['**/*.mjs'],
+        files: ['**/*.ts'],
         languageOptions: {
             parser: tsParser,
             parserOptions: {
